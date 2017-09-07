521966fc21f204adf1787d5189bd470d945b9d98
Emmanuel Jucaban <EmmanuelJucaban@gmail.com>
2017-09-06 22:58:11 -0700

    Created driver update functionality and tests for updates

--- routes/routes.js
+++ routes/routes.js
@@ -3,4 +3,5 @@ const DriversController = require('../controllers/drivers_controller');
 module.exports = (app) => {
   app.get('/api', DriversController.greeting);
   app.post('/api/drivers', DriversController.create);
+  app.put('/api/drivers/:id', DriversController.edit);
 };
--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -5,7 +5,7 @@ const app = require('../../app');
 const Driver = mongoose.model('driver');
 
 describe('Drivers Controller', () => {
-  it('Post to /api/drivers creates a new driver', (done) => {
+  it('Post to /api/drivers creates a new driver', done => {
     Driver.count().then((count) => {
       request(app)
         .post('/api/drivers')
@@ -15,6 +15,23 @@ describe('Drivers Controller', () => {
             assert(count + 1 === newCount);
             done();
           });
+        });
+    });
+  });
+
+  it('PUT to /api/drivers/:id edits an existing driver', done => {
+    const driver = new Driver({ email: 't@t.com', driving: false});
+
+    driver.save().then(() => {
+      request(app)
+        .put('/api/drivers/' + driver._id)
+        .send({driving: true})
+        .end(() => {
+          Driver.findOne({email: 't@t.com'})
+            .then( driver => {
+              assert(driver.driving === true);
+              done()
+            });
         });
     });
   });
